ae54f13bcab49ccb04d4113d174657e3a761d14c
Bryan McAfee <brymcafee@gmail.com>
2017-10-23 15:05:20 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,1 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-position_taken? method here!
+# code your #position_taken? method here!
